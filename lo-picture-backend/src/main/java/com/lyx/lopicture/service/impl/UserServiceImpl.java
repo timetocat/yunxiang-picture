30d0a40198ec504c272977ce8f7ac9f3948dbbea
timetocat <3218177938@qq.com>
2025-03-19 16:51:24 +0800

    feat：记录用户登录态到 Sa-token

--- lo-picture-backend/src/main/java/com/lyx/lopicture/service/impl/UserServiceImpl.java
+++ lo-picture-backend/src/main/java/com/lyx/lopicture/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import com.lyx.lopicture.constant.UserConstant;
 import com.lyx.lopicture.exception.BusinessException;
 import com.lyx.lopicture.exception.ErrorCode;
 import com.lyx.lopicture.exception.ThrowUtils;
+import com.lyx.lopicture.manager.auth.StpKit;
 import com.lyx.lopicture.mapper.UserMapper;
 import com.lyx.lopicture.model.convert.UserConvert;
 import com.lyx.lopicture.model.dto.user.*;
@@ -98,6 +99,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         }
         // 记录用户的登录态
         request.getSession().setAttribute(UserConstant.USER_LOGIN_STATE, user);
+        // 记录用户登录态到 Sa-token，便于空间鉴权时使用，注意保证该用户信息与 SpringSession 中的信息过期时间一致
+        StpKit.SPACE.login(user.getId());
+        StpKit.SPACE.getSession().set(UserConstant.USER_LOGIN_STATE, user);
         return this.getLoginUserVO(user);
     }
 
